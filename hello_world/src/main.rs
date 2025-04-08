07c5292680828e653728455877f8ee21ef86e41c
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-07 19:04:59 -0600

    chore: cli tutorial book

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,39 @@
+/*
+ * I followed the guide from the CLI book
+ *  --> https://rust-cli.github.io/book/index.html
+ */
+
+/* tool <pattern> <filepath> */
+fn _print_environment_vars() {
+    for (k, v) in std::env::vars() {
+        println!("{k}: {v}");
+    }
+}
+
+struct CliArgument {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn _print_cli_args() {
+    println!("printing provided arguments...");
+    for arg in std::env::args() {
+        println!("{arg}");
+    }
+}
+
+fn _handle_cli_args() {
+    /* .nth is an iter funciton */
+    let pattern = std::env::args().nth(1).expect("Please provide a pattern");
+    let filepath = std::env::args().nth(2).expect("Please provide a filepath");
+
+    println!("Looking for \"{pattern}\" in {filepath}");
+}
+
 fn main() {
     println!("Hello, world!");
+    //_print_environment_vars();
+    _print_cli_args();
+    _handle_cli_args();
 }
+
