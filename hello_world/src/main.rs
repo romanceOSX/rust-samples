e83b329668921930d35f6e42ca970e0cb0c25053
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-13 00:13:51 -0600

    chore: updating generics testing

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -40,8 +40,86 @@ fn _handle_cli_args_clap() {
 fn main() {
     println!("Hello, world!");
     //_print_environment_vars();
-    _print_cli_args();
+    //_print_cli_args();
     //_handle_cli_args();
-    _handle_cli_args_clap();
+    //_handle_cli_args_clap();
+    _test_pnet();
 }
+
+/* conditionally implement a method depending on the trait */
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self{x, y}
+    }
+}
+
+/*
+ * Interfaces abstraction wrapper sample
+ */
+trait Interface {
+    fn print_info(&self);
+}
+
+struct InterfacePnet<'a> {
+    iface: &'a pnet::datalink::NetworkInterface,
+}
+
+impl<'a> InterfacePnet<'a> {
+    fn new(pnet_iface: &'a pnet::datalink::NetworkInterface) -> Self {
+        Self {
+            iface: pnet_iface,
+        }
+    }
+}
+
+impl<'a> Interface for InterfacePnet<'a> {
+    fn print_info(&self) {
+        println!("Helloo!");
+    }
+}
+
+fn interfaces<'a>() -> Vec<Box<dyn Interface>> {
+    /* construct vector of interfaces */
+    let mut interfaces: Vec<Box<dyn Interface>> = Vec::new();
+    interfaces.push(
+        Box::new(
+            InterfacePnet {
+                /* datalink::interfaces() creates an stack-allocated object */
+                //iface: &pnet::datalink::interfaces().get(0).unwrap(),
+            }
+        )
+    );
+
+    /* 
+     * from Vec<pnet::NetworkInterfaces> 
+     *   to Vec<Interface> 
+     */
+
+    interfaces
+}
+
+/* 
+ * function that expects a vector, and returns a reference value that has the same
+ * lifetime as the vector, i.e., an element of the vector
+ * note that the lifetime notation uses the same syntax as generics
+ */
+fn _test_vector_references<'a, T>(vector: &'a Vec<T>) -> &'a T {
+    vector.iter().nth(0).unwrap()
+}
+
+fn _test_pnet() {
+    let ifaces = pnet::datalink::interfaces();
+    for iface in ifaces {
+        println!("- {:?}", iface.ips);
+    }
+
+    let vec = vec![1, 2, 3, 4, 5];
+    print!("This is the first thing {}", _test_vector_references(&vec))
+}
+
 
