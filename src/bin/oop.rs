19302ada32c61bfaed8d5473649cbca9ddfc5684
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-10 19:38:05 -0600

    feat: oop

--- src/bin/oop.rs
+++ src/bin/oop.rs
@@ -1,5 +1,78 @@
 
+/* Public API */
+trait Draw {
+    fn draw(&self);
+}
+
+struct Screen {
+    components: Vec<Box<dyn Draw>>,
+}
+
+/* Implementor Button */
+struct Button {
+    width: u32,
+    height: u32,
+    label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // drawing
+        println!("drawing button!");
+    }
+}
+
+/* Implementor SelectBox */
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("drawing Select Box!");
+    }
+}
+
+impl Screen {
+    fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+
+fn test_oop_1() {
+    let screen = Screen {
+        components: vec![
+            Box::new(
+                SelectBox {
+                    width: 10,
+                    height: 10,
+                    options: vec![
+                        String::from("Sometimes"),
+                        String::from("Poor"),
+                        String::from("Standard"),
+                    ],
+            }),
+            Box::new(
+                Button {
+                    width: 10,
+                    height: 10,
+                    label: String::from("X"),
+                }
+            ),
+        ]
+    };
+
+    screen.run();
+}
+
 fn main() {
     println!("This is an OOP sample example");
+    test_oop_1();
+    println!("Finished testing...");
 }
 
