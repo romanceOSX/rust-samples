41e341da33ef98d74e62b6366a2325dc7b3ae3c6
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-12 22:54:32 -0600

    chore: pattern matchign

--- src/bin/pattern-matching/main.rs
+++ src/bin/pattern-matching/main.rs
@@ -15,9 +15,47 @@ fn places_to_use_patterns() {
 
     let (_, a, b) = (1, 2, 3);
     println!("{a}, {b}");
+
+    let my_tup  = (1, 2, 3, 4);
+
+    match my_tup {
+        (a, _, x, y) => println!("first case"),
+        x => println!("Default case"),
+    }
+    let a = if true {
+        32
+    } else { 11 } ;
+    println!("{a}");
+
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
+
+    match msg {
+        Message::Hello {
+            id: id_variable,
+        } if id_variable < 10 && id_variable > 0 => println!("Found an id in range!!!!!!!: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
 
 fn main() {
     println!("This is pattern matching rust!");
+    places_to_use_patterns();
 }
 
