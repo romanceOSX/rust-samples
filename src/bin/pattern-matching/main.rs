d624bad6b5b8c611025e73eff2b847b5fabf2b4c
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-12 20:23:04 -0600

    chore: pattern matching

--- src/bin/pattern-matching/main.rs
+++ src/bin/pattern-matching/main.rs
@@ -1,3 +1,21 @@
+struct Foo {
+    content: String,
+}
+
+fn places_to_use_patterns() {
+    // match arms
+    let opt = Some(32);
+    let foo = Some(Foo{content: String::new()});
+
+    // TODO: how does ownership applies in this scenarios
+    let a = match foo {
+        None => None,
+        Some(i) => Some(i),
+    };
+
+    let (_, a, b) = (1, 2, 3);
+    println!("{a}, {b}");
+}
 
 fn main() {
     println!("This is pattern matching rust!");
