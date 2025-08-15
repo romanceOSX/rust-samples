f33c47ca2a3f80e0c2f986b8cd02af15a08dd42f
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-14 22:39:57 -0600

    chore: test lifetimes

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,3 +1,15 @@
+trait MyCustomTrait {
+    fn my_custom_printer(&self);
+}
+
+impl<T> MyCustomTrait for T
+    where T: std::fmt::Debug,
+{
+    fn my_custom_printer(&self) {
+        println!("Uhmm.. this is my custom printer!");
+        println!("{:?}", &self);
+    }
+}
 
 struct MyStruct<A, B> {
     a: A,
@@ -13,15 +25,30 @@ impl<A, B> MyStruct<A, B> {
 
 // More specific implementation when A and B are the same type
 impl<T> MyStruct<T, T> {
-    fn describe(&self) {
+    // this is the specialization feature of rust, only available in nightly builds
+    //fn describe(&self) {
+    fn describe_foo(&self) {
         println!("Special case: both types are the same");
     }
 }
 
-fn main() {
-    let x = MyStruct { a: 1, b: 2 };
-    x.describe(); // Special case: both types are the same
+fn longer_string<'a>(str_a: &'a str, str_b: &'a str) -> &'a str {
+    if str_a > str_b { str_a } else { str_b }
+}
 
-    let y = MyStruct { a: 1, b: 2.0 };
-    y.describe(); // Generic case: two possibly different types
+fn main() {
+    32.my_custom_printer();
+
+    let a = String::from("lkdjafsljf");
+    let b = String::from ("aaa");
+
+    let longer = longer_string(&a, &b);
+    println!("The longer string is {:?}", longer);
+
+    //3.my_custom_printer();
+    //let x = MyStruct { a: 1, b: 2 };
+    //x.describe(); // Special case: both types are the same
+
+    //let y = MyStruct { a: 1, b: 2.0 };
+    //y.describe(); // Generic case: two possibly different types
 }
