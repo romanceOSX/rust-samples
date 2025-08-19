fa2c5318b91624c14026cfdfa6a452278a9353ef
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-19 15:01:34 -0600

    chore: async

--- src/bin/async/main.rs
+++ src/bin/async/main.rs
@@ -1,11 +1,19 @@
 use trpl::Html;
 
+// What happends when rust stumbles across an async function?
+//  It rewrites the function to return a single async block as the following:
 // This is the equivalent function signature:
 //  fn page_title(url: &str) -> impl Future<Output = Option<String>> {
 //      async move {
 //          ...
 //      }
 //  }
+//  In turn, an async blocks makes rust to compile into an unique anonymus data type
+//  that implements the Future trait
+//
+//  So an async function gets turned into a non-async function that returns a type
+//  that implements a Future trait associated with the return type of your code...
+//
 //  Basically wraps the function within an async move block and returns a future type
 async fn page_title(url: &str) -> Option<String> {
     let response = trpl::get(url).await;
@@ -13,6 +21,49 @@ async fn page_title(url: &str) -> Option<String> {
     Html::parse(&response_text)
         .select_first("title")
         .map(|title_element| title_element.inner_html())
+}
+
+use std::time::Duration;
+
+fn test_spawning_tasks() {
+    // async block compiles to anonymus functions, we can rewrite the following
+    trpl::run(async {
+        // not necessary
+        let task_handle1 = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("Hi number {i} from the first task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("Hi number {i} from the seconds task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+        task_handle1.await.unwrap();
+    });
+    
+    // like this
+    trpl::run(async {
+        let f1 = async {
+            for i in 1..5 {
+                println!("Hi number {i} from the first task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+        let f2 = async {
+            for i in 1..10 {
+                println!("Hi number {i} from the first task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        // in contrast to threads, we will see that this makes them run in a fixed
+        // predictable fashion, this is due to the asynchronous runtime handling the
+        // execution, in contrast with threads where the OS is in charge of the sceduling
+        // aka. the application knows more about concurrency handling
+        trpl::join(f1, f2).await;
+    });
 }
 
 fn run_async() {
@@ -28,6 +79,7 @@ fn run_async() {
 }
 
 fn main() {
+    test_spawning_tasks();
     run_async();
     println!("Running on main")
 }
